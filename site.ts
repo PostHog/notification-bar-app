33a63bf357005003cc727a1351446463aa5893a3
Marius Andra <marius.andra@gmail.com>
2022-10-14 13:27:50 +0200

    notification bar

--- site.ts
+++ site.ts
@@ -1,4 +1,9 @@
 export function inject({ config }) {
+    const localStorageKey = `notification-${config.notification}`
+    if (config.rememberClose === 'yes' && localStorage.getItem(localStorageKey)) {
+        return
+    }
+
     const style = `
         .notification-bar-container {
             min-height: 56px;
@@ -8,8 +13,8 @@ export function inject({ config }) {
             min-height: 56px;
             text-height: 24px;
             font-size: 24px;
-            color: ${config.textColor || 'white'};
-            background: ${config.backgroundColor || 'rgba(0,0,0,0.8)'};
+            color: ${config.textColor || 'default'};
+            background: ${config.backgroundColor || 'default'};
             font-weight: normal;
             font-family: -apple-system, BlinkMacSystemFont, "Inter", "Segoe UI", "Roboto", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
             text-align: center;
@@ -20,32 +25,34 @@ export function inject({ config }) {
             align-items: center;
             justify-content: center;
             cursor: pointer;
+            z-index: 9999999;
         }
         .notification-bar a {
-            color: ${config.linkColor || config.textColor || 'white'};
+            color: ${config.linkColor || config.textColor || 'default'};
         }
         ${config.cssOverride || ''}
     `
     const paragraph = Object.assign(document.createElement('p'), {
         innerHTML: config.notification,
     })
+    const notificationElementContainer = Object.assign(document.createElement('div'), {
+        className: 'notification-bar-container',
+    })
     const notificationElement = Object.assign(document.createElement('div'), {
         className: 'notification-bar',
         onclick: (e) => {
             if (!e.target.matches('a,button')) {
                 notificationElement.style.display = 'none'
+                notificationElementContainer.style.display = 'none'
+                window.localStorage.setItem(localStorageKey, 'true')
             }
         },
         title: config.buttonTitle || '',
     })
     notificationElement.append(paragraph)
     const shadow = createShadowRoot(style)
-    const notificationElementContainer = Object.assign(document.createElement('div'), {
-        className: 'notification-bar-container',
-    })
     notificationElementContainer.appendChild(notificationElement)
     shadow.appendChild(notificationElementContainer)
-
     document.body.prepend(shadow)
 }
 
